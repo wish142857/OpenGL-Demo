80480b717cd8ce1fcd98bcc29354ac7b6605c801
wish142857 <wish142857@163.com>
2020-10-09 19:01:57 +0800

    update #2

--- OpenGL-Demo/main.cpp
+++ OpenGL-Demo/main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include <GL/glut.h>  
+void Show()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glRectf(-0.1f, -0.1f, 0.5f, 0.5f);
+    glFlush();
+}
 
-using namespace std;
-
-int main() {
-	cout << "Hello, world!" << endl;
-	return 0;
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("OpenGL");
+    glutDisplayFunc(Show);
+    glutMainLoop();
+    return 0;
 }
